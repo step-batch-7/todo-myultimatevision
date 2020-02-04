4c01d24504c24a38a4e236dd2fc1ccf184918918
nagendra <=>
2020-02-04 17:00:12 +0530

    able to delete a task/todo from list
    What changes you have made?
    added functions to delete a task/todo
    
    Why this change is necessary?
    to delete a task/todo
    
    What is the impact of the change?
    it can allow to delete tasks/todos

--- public/js/home.js
+++ public/js/home.js
@@ -1,43 +1,70 @@
+const createElement = (element) => document.createElement(element);
+
+const selectElement = (element) => document.querySelector(element);
+
+const selectAllElements = (element) => document.querySelectorAll(element);
+
+const getElementById = (id) => document.getElementById(id);
+
+const getElementsByName = (name) => document.getElementsByName(name);
+
 const addItem = function () {
-  const todoItems = document.getElementById("todoItems");
-  const item = document.createElement("textarea");
-  item.setAttribute("name", "todoItem");
+  const todoItems = getElementById('todoItems');
+  const item = createElement('textarea');
+  item.setAttribute('name', 'todoItem');
   item.rows = 3;
   item.cols = 40;
   todoItems.appendChild(item);
 };
 
 const deleteItem = function () {
-  const todoItems = document.getElementById("todoItems");
-  const items = document.getElementsByName("todoItem");
-  if (items.length > 1)
+  const todoItems = getElementById('todoItems');
+  const items = getElementsByName('todoItem');
+  if (items.length > 1) {
     todoItems.removeChild(items[items.length - 1]);
+  }
 };
 
 const refreshHomePage = function (title, textareas) {
-  const todoItems = document.querySelector('#todoItems')
-  textareas[0].value = ''
+  const todoItems = selectElement('#todoItems');
+  textareas[0].value = '';
   textareas.slice(1).forEach((textarea) => {
     todoItems.removeChild(textarea);
   });
   title.value = '';
-}
+};
 
 const getContent = function () {
-  const titleTag = document.querySelector('input');
-  const textareas = Array.from(document.querySelectorAll('textarea'));
+  const titleTag = selectElement('input');
+  const textareas = Array.from(selectAllElements('textarea'));
   const tasks = textareas.map((textarea) => `tasks=${textarea.value}`);
   const title = `title=${titleTag.value}`;
-  refreshHomePage(titleTag, textareas)
+  refreshHomePage(titleTag, textareas);
   return `${title}&${tasks.join('&')}`;
-}
+};
 
-const sendHttpPOST = (url, callback) => {
-  const data = getContent();
+const sendHttpPOST = (url, message, callback) => {
   const req = new XMLHttpRequest();
   req.onload = function () {
-    if (this.status === 200) callback(this.responseText);
-  }
+    if (this.status === 200) {
+      callback(this.responseText);
+    }
+  };
   req.open('POST', url);
-  req.send(data);
-}
+  req.send(message);
+};
+
+const writeToBody = text => {
+  selectElement('body').innerHTML = text;
+};
+
+const createTodo = (id) => sendHttpPOST('createTodo', getContent(), writeToBody);
+
+const deleteTodo = (id) => sendHttpPOST('deleteTodo', `id=${id}`, writeToBody);
+
+const deleteTask = (id) => sendHttpPOST('deleteTask', `id=${id}`, writeToBody);
+
+const doneTodo = (id) => sendHttpPOST('doneTodo', `id=${id}`, writeToBody);
+
+const doneTask = (id) => sendHttpPOST('doneTask', `id=${id}`, writeToBody);
+
