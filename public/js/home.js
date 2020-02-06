67bd835777b22a761646a6cf7ea7d661377cd6c0
nagendra <=>
2020-02-06 09:07:24 +0530

    moved getTodosHtml to home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,46 +1,75 @@
+const getTaskAsHtml = function (task) {
+  const { item, id, isDone } = task;
+  return (
+    `<span class="task">
+     <span>${item}</span>
+     <div>
+     <input type="button" value="Done" onclick="markTaskAsDone(${id})"/>
+     <img src="images/trash.png" width='20px' onclick="deleteTask(${id})"/> 
+     </div>
+     </span><br/>`);
+};
+
+const getTodoAsHtml = function (todo) {
+  const { id, title, tasks } = todo;
+  return (
+    `<div class='todo' id="${id}">
+       <div class="header"> <b>${title.toUpperCase()}</b><div> 
+       <input type="button" value="Done" onclick="markTodoAsDone(${id})"/>
+       <img src="images/trash.png" width='20px' onclick="deleteTodo(${id})"/>
+       </div>
+     </div>
+     <ul>
+       ${ tasks.map(getTaskAsHtml).join('\n')}
+     </ul>
+   </div>`);
+};
+
+const getTodosHtml = function (todoList) {
+  return todoList.map(getTodoAsHtml);
+};
+
 const createElement = (element) => document.createElement(element);
 
-const selectElement = (element) => document.querySelector(element);
+const getElement = (element) => document.querySelector(element);
 
-const selectAllElements = (element) => document.querySelectorAll(element);
-
-const getElementById = (id) => document.getElementById(id);
-
-const getElementsByName = (name) => document.getElementsByName(name);
+const getAllElements = (element) => document.querySelectorAll(element);
 
 const addItem = function () {
-  const todoItems = getElementById('todoItems');
-  const item = createElement('textarea');
+  const todoItems = getElement('#todoItems');
+  const item = createElement('input');
   item.setAttribute('name', 'todoItem');
-  item.rows = 3;
-  item.cols = 40;
+  item.setAttribute('type', 'text');
+  item.setAttribute('placeholder', 'Enter Task');
+  item.setAttribute('class', 'inputItems');
+  item.setAttribute('autocomplete', 'off');
   todoItems.appendChild(item);
 };
 
 const deleteItem = function () {
-  const todoItems = getElementById('todoItems');
-  const items = getElementsByName('todoItem');
-  if (items.length > 1) {
-    todoItems.removeChild(items[items.length - 1]);
-  }
+  const todoItems = getElement('#todoItems');
+  const items = getAllElements('.inputItems');
+  todoItems.removeChild(items[items.length - 1]);
 };
 
-const refreshHomePage = function (title, textareas) {
-  const todoItems = selectElement('#todoItems');
-  textareas[0].value = '';
-  textareas.slice(1).forEach((textarea) => {
-    todoItems.removeChild(textarea);
-  });
+const refreshHomePage = function (title, inputItems) {
+  const todoItems = getElement('#todoItems');
+  inputItems.forEach((inputItem) => todoItems.removeChild(inputItem));
   title.value = '';
 };
 
 const getContent = function () {
-  const titleTag = selectElement('input');
-  const textareas = Array.from(selectAllElements('textarea'));
-  const tasks = textareas.map((textarea) => `tasks=${textarea.value}`);
+  const titleTag = getElement('input');
+  const inputItems = Array.from(getAllElements('.inputItems'));
+  const tasksTodo = inputItems.filter((inputItem) => inputItem.value !== '');
+  const tasks = tasksTodo.map((task) => `tasks=${task.value}`);
   const title = `title=${titleTag.value}`;
-  refreshHomePage(titleTag, textareas);
-  return `${title}&${tasks.join('&')}`;
+  refreshHomePage(titleTag, inputItems);
+  if (tasks.length !== 0) {
+    console.log(`${title}&${tasks.join('&')}`);
+    return `${title}&${tasks.join('&')}`;
+  }
+  return title;
 };
 
 const sendHttpPOST = (url, message, callback) => {
@@ -66,7 +95,7 @@ const sendHttpGET = (url, callback) => {
 };
 
 const writeToBody = text => {
-  getElementById('todos').innerHTML = getTodosHtml(JSON.parse(text));
+  getElement('#todos').innerHTML = getTodosHtml(JSON.parse(text));
 };
 
 const loadTodos = () => sendHttpGET('serveTodos', writeToBody);
@@ -77,7 +106,9 @@ const deleteTodo = (id) => sendHttpPOST('deleteTodo', `id=${id}`, writeToBody);
 
 const deleteTask = (id) => sendHttpPOST('deleteTask', `id=${id}`, writeToBody);
 
-const markTodoAsDone = (id) => sendHttpPOST('markTodoAsDone', `id=${id}`, writeToBody);
+const markTodoAsDone = (id) =>
+  sendHttpPOST('markTodoAsDone', `id=${id}`, writeToBody);
 
-const markTaskAsDone = (id) => sendHttpPOST('markTaskAsDone', `id=${id}`, writeToBody);
+const markTaskAsDone = (id) =>
+  sendHttpPOST('markTaskAsDone', `id=${id}`, writeToBody);
 
