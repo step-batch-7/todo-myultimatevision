264a4e29c65fd1ed88123f9f64a7e924297f7b35
nagendra <=>
2020-02-08 11:14:39 +0530

    changed callbacks for post requests in home.js
    What changes you have made?
    changed callbacks for post requests
    
    Why this change is necessary?
    previously it is reloading entire todo list
    
    What is the impact of the change?
    it is now reloading modified elements only

--- public/js/home.js
+++ public/js/home.js
@@ -1,39 +1,48 @@
-const getTaskAsHtml = function (task) {
+const getTaskBody = function (task) {
   const { item, id, isDone } = task;
   const [cName, check] = isDone ? ['class="lightText"', 'checked'] : ['', ''];
   const checkOnclick = `onclick = "toggleTaskDoneStatus(${id})"`;
   const buttonOnclick = `onclick="performDelete(${id},'task'`;
   return (
-    `<span class="task">
-      <span>
-        <input type="checkbox" ${checkOnclick} ${check} />
-        <span ${cName} contenteditable="true" onfocusout="renameTask(${id})" id="task${id}">
-        ${item}</span>
-      </span>
-      <div>
-        <input class="delete" type="button" value="delete" ${buttonOnclick})" />
-      </div>
+    `<span>
+      <input type="checkbox" ${checkOnclick} ${check} />
+     <span ${cName} contenteditable="true" onfocusout="renameTask(${id})" id="item${id}">
+      ${item}</span>
+     </span>
+    <input class="deleteBtn" type="button" value="delete" ${buttonOnclick})" />`);
+};
+
+const getTaskAsHtml = function (task) {
+  return (
+    `<span class="task" id="task${task.id}">
+      ${getTaskBody(task)}
      </span><br/>`);
 };
 
-const getTodoAsHtml = function (todo) {
+
+const getTodoBody = function (todo) {
   const { id, title, tasks } = todo;
   const tasksDone = tasks.filter(task => task.isDone);
   const image = 'plus.svg';
   return (
-    `<div class='todo' id="id${id}">
-       <div class="header">
-      <b>${tasksDone.length}/${tasks.length}</b>
-         <b contenteditable="true" onfocusout="renameTitle(${id})" id="title${id}">
-         ${title}</b>
-         <div> 
-           <img src="images/edit.svg" onclick="focusonInput(${id})"/>
-           <img src="images/${image}" onclick="addInput(${id})"/>
-           <img src="images/trash.svg" onclick="performDelete(${id},'todo')"/>
-         </div>
-       </div>
-       <div class="addItem" id="input${id}"></div>
-         ${ tasks.map(getTaskAsHtml).join('\n')}
+    `<div class="header">
+     <b class="noOfTasks${id}">${tasksDone.length}/${tasks.length}</b>
+     <b contenteditable="true" onfocusout="renameTitle(${id})" id="title${id}">
+      ${title}</b>
+     <div>
+      <img src="images/edit.svg" onclick="focusonInput(${id})" />
+      <img src="images/${image}" onclick="addInput(${id})" />
+      <img src="images/trash.svg" onclick="performDelete(${id},'todo')" />
+     </div>
+   </div>
+    <div class="addItem" id="input${id}"></div>
+      ${ tasks.map(getTaskAsHtml).join('\n')}`);
+}
+
+const getTodoAsHtml = function (todo) {
+  return (
+    `<div class='todo' id="id${todo.id}">
+       ${getTodoBody(todo)}
      </div>`);
 };
 
@@ -82,33 +91,64 @@ const writeToBody = text => {
 
 const loadTodos = () => sendHttpGET('serveTodos', writeToBody);
 
-const createTodo = () => sendHttpPOST('createTodo', getContent(), writeToBody);
+const createTodo = () => sendHttpPOST('createTodo', getContent(), text => {
+  const todo = JSON.parse(text);
+  const div = createElement('div');
+  div.setAttribute('id', `todo${todo.id}`);
+  div.innerHTML = getTodoAsHtml(todo)
+  getElement('#todos').prepend(div);
+});
 
-const deleteTodo = (id) => sendHttpPOST('deleteTodo', `id=${id}`, writeToBody);
+const deleteTodo = (id) => sendHttpPOST('deleteTodo', `id=${id}`, () => {
+  getElement('#todos').removeChild(getElement(`#id${id}`));
+});
 
-const deleteTask = (id) => sendHttpPOST('deleteTask', `id=${id}`, writeToBody);
-
-const toggleTodoDoneStatus = (id) =>
-  sendHttpPOST('toggleTodoDoneStatus', `id=${id}`, writeToBody);
+const deleteTask = (id) => sendHttpPOST('deleteTask', `id=${id}`, () => {
+  const task = getElement(`#task${id}`);
+  const todo = task.parentElement;
+  todo.removeChild(task);
+});
 
 const toggleTaskDoneStatus = (id) =>
-  sendHttpPOST('toggleTaskDoneStatus', `id=${id}`, writeToBody);
+  sendHttpPOST('toggleTaskDoneStatus', `id=${id}`, (text) => {
+    const task = getElement(JSON.parse(text).id);
+    const checkBox = task.previousSibling;
+    if (checkBox.checked) {
+      checkBox.setAttribute('checked', false);
+      return;
+    }
+    checkBox.setAttribute('checked', true);
+  });
 
 const renameTitle = (id) => {
-  const title = getElement(`#title${id}`).innerText;
-  const data = `id=${id}&title=${title}`;
-  sendHttpPOST('renameTitle', data, writeToBody);
+  const title = getElement(`#title${id}`);
+  const data = `id=${id}&title=${title.innerText}`;
+  sendHttpPOST('renameTitle', data, () => {
+    title.innerText = title.innerText;
+  });
 };
 
 const renameTask = (id) => {
-  const task = getElement(`#task${id}`).innerText;
-  const data = `id=${id}&task=${task}`;
-  sendHttpPOST('renameTask', data, writeToBody);
+  const task = getElement(`#item${id}`);
+  const item = task.innerText;
+  const data = `id=${id}&task=${item}`;
+  sendHttpPOST('renameTask', data, () => {
+    task.innerText = item;
+  });
 };
 
 const addTask = function (id) {
-  const task = getElement(`#input${id}`).firstElementChild.value;
-  sendHttpPOST('addTask', `id=${id}&&task=${task}`, writeToBody);
+  const todo = getElement(`#id${id}`);
+  const inputBox = getElement(`#inputBox${id}`);
+  const task = inputBox.firstElementChild.value;
+  getElement(`#input${id}`).removeChild(inputBox);
+  sendHttpPOST('addTask', `id=${id}&&task=${task}`, (task) => {
+    const span = createElement('span');
+    span.setAttribute('class', 'task');
+    span.setAttribute('id', `task${id}`);
+    span.innerHTML = getTaskBody(JSON.parse(task));
+    todo.appendChild(span);
+  });
 };
 
 const performDelete = (id, item) => {
@@ -141,24 +181,17 @@ const addInputBox = (taskAdder) => {
 };
 
 const addInput = (id) => {
-  const taskAdder = getElement(`#input${id}`);
+  const inputArea = getElement(`#input${id}`);
+  const taskAdder = createElement(`div`);
+  inputArea.appendChild(taskAdder);
+  taskAdder.setAttribute('id', `inputBox${id}`)
   addInputBox(taskAdder);
   addButton(taskAdder, id);
 };
 
 const focusonInput = (id) => {
   const title = getElement(`#title${id}`);
-  const value = title.innerText;
   title.focus();
-  title.value = '';
-  title.value = value;
-}
+};
 
-const focusonTask = (id) => {
-  const title = getElement(`#task${id}`);
-  const value = title.innerText;
-  title.focus();
-  title.value = '';
-  title.value = value;
-}
 
