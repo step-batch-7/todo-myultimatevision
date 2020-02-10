1a2e41ab0f049d5868ffe8e0818ad16df19cc664
nagendra <=>
2020-02-10 11:50:39 +0530

    corrected tests

--- test/testHandlers.js
+++ test/testHandlers.js
@@ -8,19 +8,21 @@ describe('GET', function () {
       request(app.serve.bind(app))
         .get('/')
         .set('Accept', '*/*')
+        .set('Cookie', '')
         .expect(200)
         .expect('Content-Type', 'text/html')
-        .expect('Content-Length', '808')
-        .expect(/Todo/, done);
+        .expect('Content-Length', '801')
+        .expect(/Login/, done);
     });
 
     it('should get the home page when /home.html', function (done) {
       request(app.serve.bind(app))
         .get('/home.html')
         .set('Accept', '*/*')
+        .set('Cookie', 'user=sai; password=sai@16')
         .expect(200)
         .expect('Content-Type', 'text/html')
-        .expect('Content-Length', '808')
+        .expect('Content-Length', '1065')
         .expect(/Todo/, done);
     });
   });
