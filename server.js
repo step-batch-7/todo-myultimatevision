04135834fd9349484e4ee8f497a0aeb7d699f049
nagendra <=>
2020-03-02 09:29:39 +0530

    added environment variable for port

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 const { app } = require('./lib/handlers');
 
-const main = function (port = 4000) {
+const port = process.env;
+
+const main = function(port = 4000) {
   const server = new http.Server(app.serve.bind(app));
   server.listen(port, () => console.log(`listening to ${port}`));
 };
 
-main(process.argv[2]);
+main(process.argv[2] || port);
